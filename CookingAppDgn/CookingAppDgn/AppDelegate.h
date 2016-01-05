b6f47aa22146ad424edeb4ac92502948a0eee9e7
tcz00 <terrcruss@gmail.com>
2016-01-05 17:46:31 -0500

    Initial project Submission

---
+++ CookingAppDgn/CookingAppDgn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CookingAppDgn
+//
+//  Created by iOS on 1/4/16.
+//  Copyright © 2016 unlimited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CookingAppDgn/CookingAppDgn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CookingAppDgn
+//
+//  Created by iOS on 1/4/16.
+//  Copyright © 2016 unlimited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
